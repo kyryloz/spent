67dfba109f49f27447a0dd4f3d2155ee23f95c63
Kyrylo Zapylaiev <zapylaev@gmail.com>
2018-10-30 12:06:34 +0200

    Update grammar tests

--- src/parser/tests/parser.test.ts
+++ src/parser/tests/parser.test.ts
@@ -1,102 +1,96 @@
 import { parse } from '../parser'
 
 describe('parser.ts', () => {
-  describe('parse', () => {
-    describe('expense', () => {
+  describe('parse()', () => {
+    describe('legal identifier', () => {
       test('case1', () => {
-        const input = 'expense'
+        const input = 'create account My Clothes'
         expect(parse(input).succeeded()).toBeFalsy()
       })
 
       test('case2', () => {
-        const input = 'expense 100'
-        expect(parse(input).succeeded()).toBeFalsy()
+        const input = "create account 'My Clothes'"
+        expect(parse(input).succeeded()).toBeTruthy()
       })
 
       test('case3', () => {
-        const input = 'expense 200 on'
-        expect(parse(input).succeeded()).toBeFalsy()
-      })
-      test('case4', () => {
-        const input = 'expense 200 on clothes'
+        const input = "expense 100 on '  My  Clothes  '"
         expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case5', () => {
-        const input = "expense 200 on 'clothes'"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case6', () => {
-        const input = "expense 200 on 'My Clothes'"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case6', () => {
-        const input = 'expense 200 on My Clothes'
-        expect(parse(input).succeeded()).toBeFalsy()
-      })
-
-      test('case7', () => {
-        const input = "expense 200.25 on 'My Clothes'"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case8', () => {
-        const input = "expense 100 on '  My Clothes  '"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-    })
-
-    describe('create', () => {
-      test('case1', () => {
-        const input = 'create'
-        expect(parse(input).succeeded()).toBeFalsy()
-      })
-
-      test('case2', () => {
-        const input = 'create account'
-        expect(parse(input).succeeded()).toBeFalsy()
-      })
-
-      test('case3', () => {
-        const input = 'create category'
-        expect(parse(input).succeeded()).toBeFalsy()
       })
 
       test('case4', () => {
-        const input = 'create account account'
-        expect(parse(input).succeeded()).toBeFalsy()
-      })
-
-      test('case5', () => {
-        const input = 'create account account1'
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case6', () => {
         const input = 'create account account_123asd'
         expect(parse(input).succeeded()).toBeTruthy()
       })
 
-      test('case7', () => {
-        const input = "create account 'My Account'"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case8', () => {
-        const input = "createaccount ' My Account '"
-        expect(parse(input).succeeded()).toBeTruthy()
-      })
-
-      test('case9', () => {
-        const input = 'create account My Account'
+      test('case5', () => {
+        const input = "create account '1My Account'"
         expect(parse(input).succeeded()).toBeFalsy()
       })
 
-      test('case10', () => {
+      test('case6', () => {
         const input = 'create account account_#@-123asd'
         expect(parse(input).succeeded()).toBeTruthy()
+      })
+    })
+
+    describe('commands', () => {
+      describe('expense', () => {
+        test('case1', () => {
+          const input = 'expense'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+
+        test('case2', () => {
+          const input = 'expense 100'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+
+        test('case3', () => {
+          const input = 'expense 200 on'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+        test('case4', () => {
+          const input = 'expense 200 on clothes'
+          expect(parse(input).succeeded()).toBeTruthy()
+        })
+
+        test('case5', () => {
+          const input = "expense 200 on 'My Clothes'"
+          expect(parse(input).succeeded()).toBeTruthy()
+        })
+
+        test('case6', () => {
+          const input = "expense 200.25 on 'My Clothes'"
+          expect(parse(input).succeeded()).toBeTruthy()
+        })
+      })
+
+      describe('create', () => {
+        test('case1', () => {
+          const input = 'create'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+
+        test('case2', () => {
+          const input = 'create account'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+
+        test('case3', () => {
+          const input = 'create category'
+          expect(parse(input).succeeded()).toBeFalsy()
+        })
+
+        test('case4', () => {
+          const input = 'create account Wallet'
+          expect(parse(input).succeeded()).toBeTruthy()
+        })
+
+        test('case5', () => {
+          const input = "create account 'My Wallet'"
+          expect(parse(input).succeeded()).toBeTruthy()
+        })
       })
     })
   })
