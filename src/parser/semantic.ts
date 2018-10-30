a75f44be908a335080bc1208b1fd1df3039b9cc3
Kyrylo Zapylaiev <zapylaev@gmail.com>
2018-10-30 10:34:57 +0200

    Remove redundant parsers

--- src/parser/semantic.ts
+++ src/parser/semantic.ts
@@ -1,20 +1,18 @@
-import ohm from 'ohm-js'
+import * as ohm from 'ohm-js'
 import { grammar } from './grammar'
 
 const semantic = grammar.createSemantics()
 
 semantic.addOperation('eval', {
-  Exp: function(e) {
-    return e.allOperations()
-  },
+  Exp: e => e.eval(),
   Create: (_, entity, name) => {
     console.log(`Create ${entity.sourceString} with name ${name.sourceString}`)
   },
   Expense: (_, value, _0, name) => {
     console.log(`Create expense ($${value.sourceString}) on ${name.sourceString}`)
   },
-  name: () => this.sourceString,
-  value: () => parseInt(this.sourceString),
+  name: node => node.sourceString,
+  value: node => parseInt(node.sourceString),
 })
 
 export const runSemantic = (match: ohm.MatchResult) => {
