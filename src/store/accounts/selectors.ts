c0542cffed183706f46044cc65c9596d91cf6558
Kyrylo Zapylaiev <zapylaev@gmail.com>
2018-11-09 17:13:10 +0200

    Add income, expense selectors

--- src/store/accounts/selectors.ts
+++ src/store/accounts/selectors.ts
@@ -1,5 +1,7 @@
-import { App } from '../interface'
 import { createSelector } from 'reselect'
+import { Commands } from '../commands/interface'
+import { commandsSelector } from '../commands/selectors'
+import { App } from '../interface'
 
 export namespace accountsSelector {
   const byId = (state: App.State) => state.entities.accounts.byId
@@ -12,4 +14,33 @@ export namespace accountsSelector {
     })
 
   export const findById = (id: string) => createSelector(byId, resultById => resultById[id].name)
+
+  export const income = (accountId: string, timestampFrom: number, timestampTo: number) =>
+    createSelector([byId, commandsSelector.items], (byId, items) => {
+      return byId[accountId].commandIds
+        .map(commandId => items.find(item => item.id === commandId)!)
+        .filter(command => command.data.dataType === Commands.DataType.INCOME)
+        .map(data => data as Commands.IncomeData)
+        .filter(data => data.timestamp >= timestampFrom && data.timestamp < timestampTo)
+        .reduce((sum, next) => (sum += next.data.amount), 0)
+    })
+
+  export const expense = (accountId: string, timestampFrom: number, timestampTo: number) =>
+    createSelector([byId, commandsSelector.items], (byId, items) => {
+      return byId[accountId].commandIds
+        .map(commandId => items.find(item => item.id === commandId)!)
+        .filter(command => command.data.dataType === Commands.DataType.EXPENSE)
+        .map(data => data as Commands.ExpenseData)
+        .filter(data => data.timestamp >= timestampFrom && data.timestamp < timestampTo)
+        .reduce((sum, next) => (sum += next.data.amount), 0)
+    })
+
+  export const balance = (accountId: string, timestampFrom: number, timestampTo: number) =>
+    createSelector(
+      [
+        income(accountId, timestampFrom, timestampTo),
+        expense(accountId, timestampFrom, timestampTo),
+      ],
+      (income, expense) => income - expense
+    )
 }
--- src/store/commands/selectors.ts
+++ src/store/commands/selectors.ts
@@ -1,24 +1,5 @@
-import { createSelector } from 'reselect'
 import { App } from '../interface'
-import { Commands } from './interface'
 
 export namespace commandsSelector {
   export const items = (state: App.State) => state.commands.items
-
-  export const balance = (accountId: string) =>
-    createSelector(items, items => {
-      const totalExpenses = items
-        .filter(command => command.data.dataType === Commands.DataType.EXPENSE)
-        .map(command => command as Commands.ExpenseData)
-        .filter(command => command.data.accountId === accountId)
-        .reduce((sum, next) => (sum += next.data.amount), 0)
-
-      const totalIncome = items
-        .filter(command => command.data.dataType === Commands.DataType.INCOME)
-        .map(data => data as Commands.IncomeData)
-        .filter(data => data.data.accountId === accountId)
-        .reduce((sum, next) => (sum += next.data.amount), 0)
-
-      return totalIncome - totalExpenses
-    })
 }
